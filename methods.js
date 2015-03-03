01404b0e04bac52fed261a3c4dfc65a7404d665d
schahriar <info@schahriar.com>
2015-03-02 23:52:46 -0800

    Added push and auto methods

--- methods.js
+++ methods.js
@@ -1,5 +1,10 @@
 var _ = require("lodash");
 var ipaddr = require('ipaddr.js');
+var moment = require('moment');
+
+/*
+    TODO: Add ~async~ module and make all methods async
+*/
 
 var lookup = function(ip) {
     /* Lookup function */
@@ -27,17 +32,49 @@ var lookup = function(ip) {
     return lookup;
 };
 
-var admit = function(lookup){
-    /* Admissin function */
+var admit = function(lookup) {
+    /* Admission function */
     var _this = this;
 
     // Check if blocked
     if(lookup.rule.block) return false;
 
     /* Rate limiting function */
+
+    var rateRule = lookup.rule.rate;
+    var rateTotal = { h:0, m:0, s:0 };
+
+    // If any rates are set to 0
+    if((rateRule.h === 0) || (rateRule.m === 0) || (rateRule.s === 0)) return false;
+
+    // Foreach access granted time
+    _.each(lookup.location.rate, function(accessTime) {
+        if((rateRule.h) && (moment(accessTime).isBetween(moment().subtract(1, 'hours'), moment(), 'hour'))) rateTotal.h += 1;
+        if((rateRule.m) && (moment(accessTime).isBetween(moment().subtract(1, 'minutes'), moment(), 'minute'))) rateTotal.m += 1;
+        if((rateRule.s) && (moment(accessTime).isBetween(moment().subtract(1, 'seconds'), moment(), 'second'))) rateTotal.s += 1;
+    })
+
+    // If rate exceeds the limit
+    if((rateRule.h < rateTotal.h) || (rateRule.m < rateTotal.m) || (rateRule.s < rateTotal.s)) return false;
+
+    // Otherwise
+    return true;
+}
+
+var push = function(lookup) {
+    lookup.location.rate.push(moment().toISOString());
+}
+
+var auto = function(ip) {
+    var lookup = lookup(ip);
+    if(admit(lookup)) push(lookup);
+
+    return admit(lookup);
 }
 
 module.exports = {
     lookup: lookup,
-    admit: admit
+    admit: admit,
+    push: push,
+    auto: auto
 }
