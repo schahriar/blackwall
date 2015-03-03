b8df13752f1174a4bb6faa6c07486653dd3d8e39
schahriar <info@schahriar.com>
2015-03-02 17:21:42 -0800

    Added missing commas

--- policies/new.js
+++ policies/new.js
@@ -5,7 +5,7 @@ module.exports = {
         global: { name: 'global', members: new Object },
         blacklist: { name: 'blacklist', members: new Object },
         whitelist: { name: 'whitelist', members: new Object }
-    }
+    },
 
     policies: new Object
 }
