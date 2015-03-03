b14f21c3436c339dbfb8dc3fb433b3980809867d
schahriar <info@schahriar.com>
2015-03-03 14:11:14 -0800

    Created a Mocha test suite

--- test.js
+++ test.js
@@ -1,21 +1,66 @@
-var blackwall = require("./blackwall");
+var chai = require("chai");
+var BlackWall = require("./blackwall");
 var inspect = require("util").inspect;
-var firewall = new blackwall();
 
-firewall.addMember("global","2001:0db8::0001");
+var _ = require("lodash");
 
-var count = 0;
+var should = chai.should();
+var expect = chai.expect;
 
-for(i=0; i<31; i++) {
-  count++;
-  console.log(count, firewall.admit("2001:0db8::0001"));
+var firewall = new BlackWall();
+var policies = firewall.policy;
+
+var ipv6 = {
+    valid: "2001:0db8::0001",
+    expanded: "2001:db8:0:0:0:0:0:1",
+    invalid: "4001:0db8:::0001"
 }
 
-setTimeout(function(){
-  for(i=0; i<31; i++) {
-    count++;
-    console.log(count, firewall.admit("2001:0db8::0001"));
-  }
-  console.log(inspect(firewall.policy.lists.global.members, {color:true}));
-  console.log("LENGTH", firewall.policy.lists.global.members.length);
-}, 6000);
+var list = {
+    valid: "global",
+    invalid: "DNE",
+    case: "blackList"
+}
+
+var rules = {
+    basic: { rate: { d:0, h:undefined, m:undefined }, block: false },
+}
+
+describe('BlackWall Test Suite', function(){
+	describe('Member & list management checks', function(){
+		it('should add member to specified list', function(){
+            // Add member
+            firewall.addMember(list.valid,ipv6.expanded);
+            // Check for member
+            expect(policies.lists[list.valid].members[ipv6.expanded]).to.be.an('Object');
+		})
+        it('should expand ipv6 members when added to specified list', function(){
+            // Add member
+            firewall.addMember(list.valid,ipv6.valid);
+            // Check for member
+            expect(policies.lists[list.valid].members[ipv6.expanded]).to.be.an('Object');
+		})
+        it('should return an error when users are added to non-existent list', function(){
+            // Add member
+            expect(firewall.addMember(list.invalid,ipv6.expanded)).to.have.property('error');
+		})
+        it('should return an error with invalid ip addresses', function(){
+            // Add member
+            expect(firewall.addMember(list.valid,ipv6.invalid)).to.have.property('error');
+		})
+        it('should store all lists as lowercase', function(){
+            // Add list
+            firewall.addList(list.case, rules.basic, 0.5);
+            // Expect lowercase
+            expect(policies.lists[list.case.toLowerCase()]).to.have.property('name').equal(list.case.toLowerCase());
+		})
+        it('should return an error for duplicate lists', function(){
+            // Add list
+            expect(firewall.addList(list.case, rules.basic, 0.5)).to.have.property('error');
+		})
+        it('should not return an error for duplicate lists when forced', function(){
+            // Add list
+            expect(firewall.addList(list.case, rules.basic, 0.5, true)).to.not.have.property('error');
+		})
+	})
+});
