7934d9cadb3c00b9f706ffb79936ccad2c014052
schahriar <info@schahriar.com>
2015-03-03 00:21:47 -0800

    Replaced time range check

--- methods.js
+++ methods.js
@@ -49,9 +49,9 @@ var admit = function(lookup) {
 
     // Foreach access granted time
     _.each(lookup.location.rate, function(accessTime) {
-        if((rateRule.d) && (moment(accessTime).isBetween(moment().subtract(1, 'days'), moment(), 'day'))) rateTotal.d += 1;
-        if((rateRule.h) && (moment(accessTime).isBetween(moment().subtract(1, 'hours'), moment(), 'hour'))) rateTotal.h += 1;
-        if((rateRule.m) && (moment(accessTime).isBetween(moment().subtract(1, 'minutes'), moment(), 'minute'))) rateTotal.m += 1;
+        if((rateRule.d) && (moment(accessTime).diff(moment(), 'days') <= 1 ) rateTotal.d += 1;
+        if((rateRule.h) && (moment(accessTime).diff(moment(), 'hours') <= 1 ) rateTotal.h += 1;
+        if((rateRule.m) && (moment(accessTime).diff(moment(), 'minutes') <= 1 ) rateTotal.m += 1;
     })
 
     // Log for testing
