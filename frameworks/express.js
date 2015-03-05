91be4eb761bb41d8a04455e0c2e79eedfd27f9a2
schahriar <info@schahriar.com>
2015-03-04 16:22:13 -0800

    Updated express framework

--- frameworks/express.js
+++ frameworks/express.js
@@ -1,8 +1,15 @@
 var _ = require("lodash");
 module.exports = {
-    create: function(req, res, next) {
-        // Perhaps in future versions Blackwall could request for all ips in one go    
-        if( this.admit(_.first(req.ips)) ) next();
-        else res.status(503).end();
-    }
+    name: "express",
+    framework: true,
+    
+    create: function() {
+        var _this = this;
+
+        return function(req, res, next) {
+            // Perhaps in future versions Blackwall could request for all ips in one go
+            if( _this.admit(_.first(req.ips) || req.ip) === true ) next();
+            else res.status(503).end();
+        }
+    },
 }
