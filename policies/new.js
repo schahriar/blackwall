d1d4959d96cd23ad1dfe85337c05bea32f66619f
schahriar <info@schahriar.com>
2015-03-04 16:22:34 -0800

    Removed default lists from new policy

--- policies/new.js
+++ policies/new.js
@@ -1,11 +1,7 @@
 module.exports = {
     name: undefined,
 
-    lists : {
-        global: { name: 'global', priority:0, members: new Object },
-        blacklist: { name: 'blacklist', priority: 1, members: new Object },
-        whitelist: { name: 'whitelist', priority: 0.9, members: new Object }
-    },
+    lists : new Object,
 
     policies: new Object
 }
