3874c7a4ee63a2ab3c1433ffa41d2607acc6c67e
schahriar <info@schahriar.com>
2015-03-06 14:17:18 -0800

    Added override ip option for express framework

--- frameworks/express.js
+++ frameworks/express.js
@@ -3,12 +3,13 @@ module.exports = {
     name: "express",
     framework: true,
 
-    inbound: function() {
+    inbound: function(options) {
         var _this = this;
 
         return function(req, res, next) {
+            if(!options.unsafe) req.overrideip = false;
             // Perhaps in future versions Blackwall could request for all ips in one go
-            _this.session((_.first(req.ips) || req.ip), function(error, hasAccess) {
+            _this.session( (req.overrideip || _.first(req.ips) || req.ip), function(error, hasAccess) {
                 if(error) res.status(500).end(); // Add error reporting in development env
 
                 if(hasAccess === true) next();
