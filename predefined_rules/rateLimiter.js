608dc06abd6bd597164a88f1c92af579c8694e22
Schahriar SaffarShargh <info@schahriar.com>
2015-10-12 13:25:04 -0700

    RateLimiter predefined rule is now async

--- predefined_rules/rateLimiter.js
+++ predefined_rules/rateLimiter.js
@@ -18,32 +18,39 @@ module.exports = {
     description: 'Limits Session Rate based on hits per hour|minute|second',
     group: 'analytics',
     func: function(options, local, analytics, callback){
-        if(!analytics.record) analytics.record = {};
-        if(!analytics.time) analytics.time = [];
-        // Push Session Time to Analytics if it doesn't exist
-        if(!analytics.record[this.sessionID]) {
-            // Analytics Record Prevents Multiple Ratelimiters to push dates for the same session
-            analytics.record[this.sessionID] = true;
-            // Push Date for this session
-            analytics.time.push(this.createdAt);
-        };
-        // Prevent Computation if no Options are passed
-        options.get('rate', function(error, rate) {
-            if(error) return callback(error);
-            // If Rate is not defined ignore
-            if(!rate) return callback(null, true);
-            if(!_.isPlainObject(rate)) return callback("Option->Rate is not an object");
-            
-            // Fast Compare
-            if(FAST_TIME_COMPARE(analytics.time[analytics.time.length - +rate.s], 1000)) {
-                callback("Max Number Of Hits Per Second Reached");
-            }else if (FAST_TIME_COMPARE(analytics.time[analytics.time.length - +rate.m], 60000)){
-                callback("Max Number Of Hits Per Minute Reached");
-            }else if (FAST_TIME_COMPARE(analytics.time[analytics.time.length - +rate.h], 3600000)){
-                callback("Max Number Of Hits Per Hour Reached");
-            }else{
-                callback(null, true);
-            }
+        var session = this;
+        analytics.get('record', function(error, record) {
+            analytics.get('time', function(error, time) {
+                if(!record) record = {};
+                if(!time) time = [];
+                // Push Session Time to Analytics if it doesn't exist
+                if(!record[session.sessionID]) {
+                    // Analytics Record Prevents Multiple Ratelimiters to push dates for the same session
+                    record[session.sessionID] = true;
+                    // Push Date for this session
+                    time.push(session.createdAt);
+                    analytics.set('time', time);
+                    analytics.set('record', record);
+                };
+                // Prevent Computation if no Options are passed
+                options.get('rate', function(error, rate) {
+                    if(error) return callback(error);
+                    // If Rate is not defined ignore
+                    if(!rate) return callback(null, true);
+                    if(!_.isPlainObject(rate)) return callback("Option->Rate is not an object");
+                    
+                    // Fast Compare
+                    if(FAST_TIME_COMPARE(time[time.length - +rate.s], 1000)) {
+                        callback("Max Number Of Hits Per Second Reached");
+                    }else if (FAST_TIME_COMPARE(time[time.length - +rate.m], 60000)){
+                        callback("Max Number Of Hits Per Minute Reached");
+                    }else if (FAST_TIME_COMPARE(time[time.length - +rate.h], 3600000)){
+                        callback("Max Number Of Hits Per Hour Reached");
+                    }else{
+                        callback(null, true);
+                    }
+                })  
+            })
         })
     }
 }
